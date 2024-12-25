46bd802fee601d6a883740ff08ae9dbdfb5b4858
Ayank0007 <ayankhan473551@gmail.com>
2024-12-25 12:03:02 +0530

    added auth using cookies and header

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,10 +1,9 @@
-
 const jwt=require('jsonwebtoken');
 require('dotenv').config();
 
 exports.auth=(req,res,next) => {
     try {
-        const token =req.body.token;
+        const token =req.body.token ||req.cookies.token || req.header('Authorization').replace('Bearer ','');
         if(!token){
             return res.status(401).json({
                 success:false,
